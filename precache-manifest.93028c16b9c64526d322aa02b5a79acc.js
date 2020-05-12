545ffeb08c0ee5fbc9d207e54bb78427ecaf1eb5
unknown <agasantoso7@gmail.com>
2020-05-12 11:12:38 +0700

    Updates

---
+++ precache-manifest.93028c16b9c64526d322aa02b5a79acc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b959eeb3aedd096712e8d2f5e095003f",
+    "url": "/Archidax/index.html"
+  },
+  {
+    "revision": "cfcfb564a94cae089269",
+    "url": "/Archidax/static/js/2.e000470f.chunk.js"
+  },
+  {
+    "revision": "065978f0783a505c6ac2a5c4db00b221",
+    "url": "/Archidax/static/js/2.e000470f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1092ebf1f35c57ec1d34",
+    "url": "/Archidax/static/js/main.2bd59d56.chunk.js"
+  },
+  {
+    "revision": "6417e6aca8893627fbf1",
+    "url": "/Archidax/static/js/runtime-main.05876ab2.js"
+  }
+]);
